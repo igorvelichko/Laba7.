90f17d500586a2048215b5500f1e22366491ebe4
igorvelichko <velichcko.228-2016@yandex.ru>
2023-04-07 20:40:27 +1000

    LABA7.

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	struct human {
+    char name[20];
+    char surname[20];
+    int year;
+};
+
 	return 0;
 }
