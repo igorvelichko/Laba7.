cc6ee8093abc40b9a6aafc893e2c2c1716e7f76b
igorvelichko <velichcko.228-2016@yandex.ru>
2023-04-08 12:57:33 +1000

    Laba7

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
-int main(int argc, char *argv[]) {
-	struct human {
+typedef struct {
     char name[20];
     char surname[20];
     int year;
-};
+}  human;
 
-	return 0;
+int sort (const void *a, const void *b){
+	human *humanA = (human *)a;
+    human *humanB = (human *)b;
+    return (humanA->year - humanB->year);
+}
+
+int main(int argc, char *argv[]) {
+	FILE *f = fopen("C:\c labs\Labs\Laba7\"input.txt", "r");
+    int count = 0;
+    char buffer[100];
+    while (fgets(buffer, 100, f) != NULL) {
+        count++;
+    }
+    fseek(f, 0, SEEK_SET);
+    
+    
 }
