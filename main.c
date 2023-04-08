2adc52ff14ecc778521179975b4490694b336f26
igorvelichko <velichcko.228-2016@yandex.ru>
2023-04-08 13:00:23 +1000

    Laba7

--- main.c
+++ main.c
@@ -15,12 +15,13 @@ int sort (const void *a, const void *b){
 
 int main(int argc, char *argv[]) {
 	FILE *f = fopen("C:\c labs\Labs\Laba7\"input.txt", "r");
-    int count = 0;
-    char buffer[100];
-    while (fgets(buffer, 100, f) != NULL) {
-        count++;
+    int x = 0;
+    char s[100];
+    while (fgets(s, 100, f) != NULL) {
+        x++;
     }
     fseek(f, 0, SEEK_SET);
-    
+    human *arr1 = malloc(sizeof(human) * x);
+    human *arr2 = malloc(sizeof(human) * x);
     
 }
