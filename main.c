ba1c6ca0eace164fc172f9ee55c1039011a14947
igorvelichko <velichcko.228-2016@yandex.ru>
2023-04-15 12:08:27 +1000

    Laba7

--- main.c
+++ main.c
@@ -21,7 +21,26 @@ int main(int argc, char *argv[]) {
         x++;
     }
     fseek(f, 0, SEEK_SET);
+    
     human *arr1 = malloc(sizeof(human) * x);
     human *arr2 = malloc(sizeof(human) * x);
     
+    printf("Данные людей, считанные из файла:\n");
+    for (int i = 0; i < count; i++) {
+        fscanf(f_input, "%s %s %d", arr1[i].name, arr1[i].surname, &arr1[i].year_of_birth);
+        printf("%s %s, %d year\n", arr1[i].surname, arr1[i].name, arr1[i].year_of_birth);
+    }
+    
+    fclose(f_input);
+    
+    memcpy(arr2, arr1, sizeof(human) * count);
+    qsort(arr2, count, sizeof(human), compare_years);
+    printf("\nMas:\n");
+    for (int i = 0; i < count; i++) {
+        printf("%s %s, %d year\n", arr2[i].surname, arr2[i].name, arr2[i].year_of_birth);
+    }
+    free(arr1);
+    free(arr2);
+
+    return 0;
 }
